28c5481758f9b09816fb87ec51d27fa5d5e816c9
Leonardo Jaques <jaques.projetos@outlook.com>
2022-08-17 21:10:45 -0300

    feat: add note array and method

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -4,24 +4,25 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea v-model="newNote" class="textarea"
+            placeholder="Add a new note" ref="newNoteRef" />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">Add new notes</button>
+          <button @click="addNote" :disabled="!newNote"
+            class="button is-link has-background-success">Add new
+            notes</button>
         </div>
 
       </div>
     </div>
   </div>
-  <div class="card mb-4">
+  <div v-for="note in notes" :key="note.id" class="card mb-4">
     <div class="card-content">
       <div class="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, modi. Vitae, reiciendis. Atque dicta nam
-        eligendi fuga sed magnam illo, officia consectetur perspiciatis? Cum, repellat quibusdam dolorem doloribus
-        doloremque voluptas!
+        {{ note.content }}
       </div>
     </div>
     <footer class="card-footer">
@@ -34,7 +35,33 @@
 </template>
 
 <script setup>
+import { ref } from "@vue/reactivity";
 
+
+const newNote = ref('')
+const newNoteRef = ref(null)
+
+const notes = ref([
+  {
+    id: "id1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, modi. Vitae, reiciendis. Atque dicta nam eligendi fuga sed magnam illo, officia consectetur perspiciatis? Cum, repellat quibusdam dolorem doloribus doloremque voluptas!",
+  },
+  {
+    id: "id2",
+    content: "This is shorter note!"
+  },
+])
+const addNote = () => {
+  let currenteDate = new Date().getTime(),
+    id = currenteDate.toString()
+  let note = {
+    id,
+    content: newNote.value,
+  }
+  notes.value.unshift(note)
+  newNote.value = ""
+  newNoteRef.value.focus()
+}
 </script>
 
 <style  scoped>
