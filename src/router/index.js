dfc71813f90aeb6e9217ad25804339a27ef4b1eb
Leonardo Jaques <jaques.projetos@outlook.com>
2022-08-22 22:03:16 -0300

    fix: use navigation guard of vue router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import ViewNotes from "@/views/ViewNotes.vue";
 import ViewStats from "@/views/ViewStats.vue";
 import ViewEditNote from "@/views/ViewEditNote.vue";
 import ViewAuth from "@/views/ViewAuth.vue";
+import { useStoreAuth } from "@/store/StoreAuth";
 
 const routes = [
   {
@@ -30,4 +31,16 @@ const routes = [
 export const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from) => {
+  const storeAuth = useStoreAuth();
+
+  if (!storeAuth.user.id && to.name !== "auth") {
+    return { name: "auth" };
+  }
+
+  if (storeAuth.user.id && to.name === "auth") {
+    return false;
+  }
 });
